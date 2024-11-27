7f40dda85d667e3019680a3a9750869e6899a446
ahmet <mcamurlu1923@gmail.com>
2024-11-27 11:44:48 +0300

    react navigation

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+const Stack = createNativeStackNavigator();
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Categories" component={CategoriesScreen} />
+    </Stack.Navigator>
+  );
+}
 export default function App() {
   return (
     <View style={styles.mainWrapper}>
-      <StatusBar style="light" />
-      <CategoriesScreen style={styles.container} />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
     </View>
   );
 }
